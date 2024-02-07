8582b2c86127ed03abe7e9e5d512c71724e2b685
dei20 <107045601+TomCP20@users.noreply.github.com>
2024-02-07 21:37:12 +0000

    Fixed path issue

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,14 +4,15 @@ import fs from "fs"
 
 const app = express()
 const PORT = 3001
-const filepath = path.join(__dirname, "..", "db", "data.json");
+const dataPath = path.join(__dirname, "..", "db", "data.json");
+const buildPath = path.join(__dirname, "..", "..", "client", "build")
 
-app.use(express.static(path.join(__dirname, "..", "..", "client", "build")))
+app.use(express.static(buildPath))
 
 app.use(express.json())
 
 app.post("/api", (req, res) => {
-    fs.writeFile(filepath, JSON.stringify(req.body, null, 4), (err) => {
+    fs.writeFile(dataPath, JSON.stringify(req.body, null, 4), (err) => {
         if (err) throw err;
     });
     res.json(req.body);
@@ -19,14 +20,14 @@ app.post("/api", (req, res) => {
 
 app.get("/api", (req, res) => {
     res.header("Content-Type",'application/json');
-    fs.readFile(filepath, 'utf8', (err, data) => {
+    fs.readFile(dataPath, 'utf8', (err, data) => {
         if (err) throw err;
         res.send(data)
     })
 })
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"))
+    res.sendFile(path.join(buildPath, "index.html"))
 })
 
 app.listen(PORT, () => console.log(`server is running at http://localhost:${PORT}.`))
