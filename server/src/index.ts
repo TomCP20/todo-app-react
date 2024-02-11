276f295b1f1894d819b37563ed6dcbe825b7e64a
dei20 <107045601+TomCP20@users.noreply.github.com>
2024-02-11 01:35:19 +0000

    refactored database generation

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,24 +2,16 @@ import express from "express"
 import path from "path"
 import fs from "fs"
 import api from "./routes/api"
-
 const app = express()
 const PORT = 3001
-const dataDirPath = path.join(__dirname, "..", "db");
-const dataPath = path.join(dataDirPath, "data.json");
+const dataPath = path.join(__dirname, "..", "db", "data.json");
 const buildPath = path.join(__dirname, "..", "..", "client", "build")
 
-if (!fs.existsSync(dataDirPath)){
-    fs.mkdirSync(dataDirPath);
-}
+fs.promises.mkdir(path.dirname(dataPath), {recursive: true})
+    .then(() => fs.promises.writeFile(dataPath, "[]", { flag: 'wx' }))
+    .catch((e) => { console.error("Database exists."); })
 
-fs.stat(dataPath, (err, stat) => {
-    if (err != null && err.code === 'ENOENT') {
-        fs.writeFile(dataPath, "[]", (err) => {
-            if (err) throw err;
-        });
-    }
-});
+
 
 app.use(express.static(buildPath))
 
